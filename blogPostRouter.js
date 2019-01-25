1ffc1935d1615c007da89c6d836f77525f8f18de
Will Shaughnessy <WillShaughnessy@MacBook-Pro.local>
2019-01-24 21:33:46 -0500

    cleaned up file

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -6,25 +6,15 @@ const jsonParser = bodyParser.json();
 
 const {BlogPosts} = require('./models');
 
-// we're going to add some items to BlogPosts
-// so there's some data to look at
 BlogPosts.create('title1', 'content1', 'author1', 'publishDate1');
 BlogPosts.create('title2', 'content2', 'author2', 'publishDate2');
 BlogPosts.create('title3', 'content3', 'author3', 'publishDate3');
 
-// when the root of this router is called with GET, return
-// all current BlogPosts items
 router.get('/', (req, res) => {
   res.json(BlogPosts.get());
 });
 
-
-// when a new shopping list item is posted, make sure it's
-// got required fields ('name' and 'checked'). if not,
-// log an error and return a 400 status code. if okay,
-// add new item to BlogPosts and return it with a 201.
 router.post('/', jsonParser, (req, res) => {
-  // ensure `name` and `budget` are in request body
   const requiredFields = ['title', 'content', 'author', 'publishDate'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -39,19 +29,13 @@ router.post('/', jsonParser, (req, res) => {
 });
 
 
-// when DELETE request comes in with an id in path,
-// try to delete that item from BlogPosts.
 router.delete('/:id', (req, res) => {
   BlogPosts.delete(req.params.id);
   console.log(`Deleted blog post \`${req.params.ID}\``);
   res.status(204).end();
 });
 
-// when PUT request comes in with updated item, ensure has
-// required fields. also ensure that item id in url path, and
-// item id in updated item object match. if problems with any
-// of that, log error and send back status code 400. otherwise
-// call `BlogPosts.update` with updated item.
+
 router.put('/:id', jsonParser, (req, res) => {
   const requiredFields = ['title', 'content', 'author', 'publishDate'];
   for (let i=0; i<requiredFields.length; i++) {
